dec0d7c4899894d5a2e5e84be9e12e2572c4c115
Sook-Yee Chong <51235784+doscsy12@users.noreply.github.com>
2024-11-29 19:08:25 +0800

    Update

--- RoboHome/P001_join.sql
+++ RoboHome/P001_join.sql
@@ -1,8 +1,10 @@
-SELECT GS.PatientID, GS.SessionID, min(TF.TimeStampDate) AS start_time, max(TF.TimeStampDate) AS end_time, GS.CreatedDate, GS.SessionTime, GS.Score, GS.Strength, GS.Coordination, GS.Agility, GS.NumberOfTargets
+SELECT GS.PatientID, GS.SessionID, min(TF.TimeStampDate) AS start_time, 
+max(TF.TimeStampDate) AS end_time, 
+GS.CreatedDate, GS.SessionTime, GS.Score, GS.Strength, GS.Coordination, GS.Agility, GS.NumberOfTargets
 FROM GameSessionModel GS
 LEFT OUTER JOIN TargetFeedbackDataModel TF ON GS.SessionID = TF.SessionID
 -- change PatientID accordingly
-WHERE GS.PatientID='TTSH-CHISEL0Pa/Gtc3DUqoPVREp+I/qg=='
+WHERE GS.PatientID='Tw/CHE2/ERGAagsae23'
 GROUP BY GS.SessionID
 ORDER BY GS.CreatedDate 
 
--- RoboHome/P001_result.sql
+++ RoboHome/P001_result.sql
@@ -1,5 +1,10 @@
-SELECT PatientID, SessionID, GameID, TimeStampDate, TimeStampMS, Sparc, VisualTargetXCoord, VisualTargetYCoord, TargetXCoord, TargetYCoord, XCoordinate, YCoordinate, KxGain, KyGain, KxyGain, BxGain, ByGain, BxyGain, ByxGain, VelocityX, VelocityY, CurrentSenseLeft, CurrentSenseRight, TotalForce
-FROM TargetFeedbackDataModel
+SELECT PatientID, SessionID, GameID, TimeStampDate, TimeStampMS, Sp, 
+VTargetXCoord, VTargetYCoord, 
+TargetXCoord, TargetYCoord, 
+XCoordinate, YCoordinate, 
+KxGain, KyGain, KxyGain, BxGain, ByGain, BxyGain, ByxGain, 
+VelocityX, VelocityY, CurrentSenseLeft, CurrentSenseRight, TotalForce
+FROM TargetFeedbackDataSet
 -- change PatientID accordingly 
 -- change start date of home-use 
-WHERE PatientID='TTSH-CHISEL0Pa/Gtc3DUqoPVREp+I/qg==' AND TimeStampDate >= '2022-03-11' 
+WHERE PatientID='T1-CH1Pa/aFaeDEGwed' AND TimeStampDate >= '2022-03-11' 
